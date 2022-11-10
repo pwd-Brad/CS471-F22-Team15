62a9b9c606d36edf3b211314c8e55a443762255d
pwd-Brad <97566595+pwd-Brad@users.noreply.github.com>
2022-11-10 15:32:59 -0700

    Closes #49 (#50)

--- server.rb
+++ server.rb
@@ -83,10 +83,21 @@ class GHAapp < Sinatra::Application
 
     # When an issue is opened, add a label
     def handle_issue_opened_event(payload)
-      repo = payload['repository']['full_name']
-      issue_number = payload['issue']['number']
-      @installation_client.add_labels_to_an_issue(repo, issue_number, ['needs-response'])
+      logger.debug 'An issue was created'
+      repo = payload["repository"]["full_name"]
+      number = payload["issue"]["number"]
+      author = payload["issue"]["user"]["login"]
+      content = payload['comment']['body']
+      message = "Looks like @" + author + " posted a new issue. You better not say any dirty words."
+      @installation_client.add_comment(repo, number, message)
     end
+
+    # When there is a comment, grab username from the comment
+    def handle_comment_event(payload)
+      #grab the username from comment
+      username = payload['comment']['user']['login']
+      #grab the content from the comment
+      content = payload['comment']['body']
 
     def parse_content(content, user)
       # Stores swearjar value for this content
@@ -99,6 +110,7 @@ class GHAapp < Sinatra::Application
         swearcount += content.scan(/#{key}/).size * value.to_f
       end
       # Look at swearjar, search for user, and add swearcount to their amount owed
+
     end
 
     def handle_comment_event(payload)
